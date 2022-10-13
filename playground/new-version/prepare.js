7228cafa1eda2a3899c86f6717f2b27228338036
alphardex <2582347430@qq.com>
2022-10-13 16:35:12 +0800

    update

--- playground/new-version/prepare.js
+++ playground/new-version/prepare.js
@@ -1,1 +1,5 @@
-const prepare = async () => {};
+const prepare = async () => {
+  const computeText = await fetchText(`./compute.glsl?v=${Math.random()}`);
+  const computeShader = computeText;
+  window.computeShader = computeShader;
+};
--- playground/new-version/sketch.js
+++ playground/new-version/sketch.js
@@ -5,106 +5,102 @@ import * as dat from "lil";
 
 class Sketch extends kokomi.Base {
   create() {
-    this.camera.position.set(0, 0, 3);
+    this.camera.position.set(0, 0, 2);
 
     new kokomi.OrbitControls(this);
 
     const params = {
-      progress: 0,
+      width: 128,
+      pointSize: 4,
+      amplitude: 0.002,
+      frequency: 1,
     };
-    this.params = params;
 
-    const cm = new kokomi.CustomMesh(this, {
-      baseMaterial: new THREE.MeshStandardMaterial(),
-      // geometry: new THREE.PlaneGeometry(1, 1).toNonIndexed(),
-      // geometry: new THREE.SphereGeometry(1, 32, 32).toNonIndexed(),
-      geometry: new THREE.IcosahedronGeometry(1, 10).toNonIndexed(),
+    const { width, pointSize, amplitude, frequency } = params;
+
+    // gpgpu
+    const gpgpu = new kokomi.GPUComputer(this, {
+      width,
+    });
+
+    // pos DataTexture
+    const posDt = gpgpu.createTexture();
+
+    const geo = new THREE.SphereGeometry(1, 128, 128);
+    const posBuffer = geo.attributes.position.array;
+    const vertCount = posBuffer.length / 3;
+
+    const posDtData = posDt.image.data;
+    kokomi.iterateBuffer(
+      posDtData,
+      posDtData.length,
+      (arr, axis) => {
+        const rand = Math.floor(Math.random() * vertCount);
+        arr[axis.x] = posBuffer[rand * 3];
+        arr[axis.y] = posBuffer[rand * 3 + 1];
+        arr[axis.z] = posBuffer[rand * 3 + 2];
+        arr[axis.w] = 1;
+      },
+      4
+    );
+
+    // pos Variable
+    const posVar = gpgpu.createVariable(
+      "texturePosition",
+      computeShader,
+      posDt,
+      {
+        uAmplitude: {
+          value: amplitude,
+        },
+        uFrequency: {
+          value: frequency,
+        },
+      }
+    );
+
+    // init gpgpu
+    gpgpu.init();
+
+    // geometry
+    const geometry = new THREE.BufferGeometry();
+    const positions = new Float32Array(width * width * 3);
+    const reference = new Float32Array(width * width * 2);
+    for (let i = 0; i < width * width; i++) {
+      const x = Math.random();
+      const y = Math.random();
+      const z = Math.random();
+      positions.set([x, y, z], i * 3);
+      const xx = (i % width) / width;
+      const yy = Math.floor(i / width) / width;
+      reference.set([xx, yy], i * 2);
+    }
+    geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute("reference", new THREE.BufferAttribute(reference, 2));
+
+    // custom points
+    const cp = new kokomi.CustomPoints(this, {
+      baseMaterial: new THREE.ShaderMaterial(),
+      geometry,
       vertexShader,
       fragmentShader,
       materialParams: {
         side: THREE.DoubleSide,
-        // wireframe: true,
       },
       uniforms: {
-        uProgress: {
-          value: params.progress,
+        uPositionTexture: {
+          value: null,
+        },
+        uPointSize: {
+          value: pointSize,
         },
       },
     });
-    cm.addExisting();
-    this.cm = cm;
-
-    // random
-    const posCount = cm.mesh.geometry.attributes.position.count;
-    const randomBuffer = kokomi.makeBuffer(posCount, () => Math.random(), 1);
-    kokomi.iterateBuffer(randomBuffer, randomBuffer.length, (arr, axis) => {
-      const rand = Math.random();
-      arr[axis.x] = rand;
-      arr[axis.y] = rand;
-      arr[axis.z] = rand;
-    });
-    cm.mesh.geometry.setAttribute(
-      "aRandom",
-      new THREE.BufferAttribute(randomBuffer, 1)
-    );
-
-    // center
-    kokomi.getPositionCentroids(cm.mesh.geometry);
-
-    // lights
-    const ambiLight = new THREE.AmbientLight(0xffffff, 0.6);
-    this.scene.add(ambiLight);
-    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-    dirLight.position.set(1, 2, 3);
-    this.scene.add(dirLight);
-
-    // transition
-    const duration = 3;
-
-    const doTransition = () => {
-      gsap.to(params, {
-        progress: 1,
-        duration,
-        ease: "power1.inOut",
-      });
-    };
-
-    const undoTransition = () => {
-      gsap.to(params, {
-        progress: 0,
-        duration,
-        ease: "power1.inOut",
-      });
-    };
-
-    this.interactionManager.add(cm.mesh);
-    cm.mesh.addEventListener("click", () => {
-      const progress = params.progress;
-      if (progress < 0.5) {
-        doTransition();
-      } else {
-        undoTransition();
-      }
-    });
+    cp.addExisting();
 
     this.update(() => {
-      cm.mesh.material.uniforms.uProgress.value = params.progress;
+      const mat = cp.points.material;
+      mat.uniforms.uPositionTexture.value = gpgpu.getVariableRt(posVar);
     });
-
-    // this.createDebug();
-  }
-  createDebug() {
-    const params = this.params;
-    const cm = this.cm;
-
-    const gui = new dat.GUI();
-    gui
-      .add(params, "progress")
-      .min(0)
-      .max(1)
-      .step(0.01)
-      .onChange((val) => {
-        cm.mesh.material.uniforms.uProgress.value = val;
-      });
   }
 }
