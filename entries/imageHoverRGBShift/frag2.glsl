7d45d1779f47752b085d216e906dd13e117d04e2
alphardex <2582347430@qq.com>
2022-10-18 14:53:02 +0800

    update

---
+++ entries/imageHoverRGBShift/frag2.glsl
@@ -0,0 +1,67 @@
+uniform float iTime;
+uniform vec2 iResolution;
+uniform vec2 iMouse;
+
+uniform sampler2D tDiffuse;
+
+varying vec2 vUv;
+
+uniform float uMaskRadius;
+uniform float uDevicePixelRatio;
+uniform vec2 uMouse;
+uniform float uMouseSpeed;
+
+float circle(vec2 st,float r,vec2 v){
+    float d=length(st-v);
+    float c=smoothstep(r-.2,r+.2,d);
+    return c;
+}
+
+float getCircle(float radius){
+    vec2 viewportP=gl_FragCoord.xy/iResolution/uDevicePixelRatio;
+    float aspect=iResolution.x/iResolution.y;
+    
+    vec2 m=iMouse.xy/iResolution.xy;
+    
+    vec2 maskP=viewportP-m;
+    maskP/=vec2(1.,aspect);
+    maskP+=m;
+    
+    float r=radius/iResolution.x;
+    float c=circle(maskP,r,m);
+    
+    return c;
+}
+
+#define GLSLIFY 1
+vec3 blackAndWhite(vec3 color){
+    return vec3((color.r+color.g+color.b)/5.);
+}
+
+vec4 RGBShift(sampler2D t,vec2 rUv,vec2 gUv,vec2 bUv,float isBlackWhite){
+    vec4 color1=texture(t,rUv);
+    vec4 color2=texture(t,gUv);
+    vec4 color3=texture(t,bUv);
+    if(isBlackWhite==1.){
+        color1.rgb=blackAndWhite(color1.rgb);
+        color2.rgb=blackAndWhite(color2.rgb);
+        color3.rgb=blackAndWhite(color3.rgb);
+    }
+    vec4 color=vec4(color1.r,color2.g,color3.b,color2.a);
+    return color;
+}
+
+void main(){
+    vec2 p=vUv;
+    
+    float mask=1.-getCircle(uMaskRadius/uDevicePixelRatio);
+    float r=mask*uMouseSpeed*.5;
+    float g=mask*uMouseSpeed*.525;
+    float b=mask*uMouseSpeed*.55;
+    vec4 tex=RGBShift(tDiffuse,p+=r,p+=g,p+=b,0.);
+    
+    // vec4 col=vec4(mask);
+    vec4 col=tex;
+    
+    gl_FragColor=col;
+}
---
+++ entries/imageHoverRGBShift/sketch.js
@@ -0,0 +1,66 @@
+import * as kokomi from "kokomi.js";
+import * as THREE from "three";
+import gsap from "gsap";
+import * as dat from "lil";
+
+class Sketch extends kokomi.Base {
+  async create() {
+    const screenCamera = new kokomi.ScreenCamera(this);
+    screenCamera.addExisting();
+
+    const gallary = new kokomi.Gallery(this, {
+      vertexShader,
+      fragmentShader,
+    });
+    await gallary.addExisting();
+
+    const customEffect = new kokomi.CustomEffect(this, {
+      vertexShader: vertexShader2,
+      fragmentShader: fragmentShader2,
+      uniforms: {
+        uMaskRadius: {
+          value: 160,
+        },
+        uDevicePixelRatio: {
+          value: window.devicePixelRatio,
+        },
+        uMouse: {
+          value: new THREE.Vector2(0, 0),
+        },
+        uMouseSpeed: {
+          value: 0,
+        },
+      },
+    });
+    customEffect.addExisting();
+
+    // mouse move
+    let offsetX = 0;
+    let offsetY = 0;
+
+    let targetSpeed = 0;
+
+    this.update(() => {
+      // mouse pos
+      const { x, y } = this.interactionManager.mouse;
+      offsetX = THREE.MathUtils.lerp(offsetX, x, 0.1);
+      offsetY = THREE.MathUtils.lerp(offsetY, y, 0.1);
+      customEffect.customPass.material.uniforms.uMouse.value =
+        new THREE.Vector2(offsetX, offsetY);
+
+      // mouse speed
+      const hoverDelta = new THREE.Vector2(
+        this.iMouse.mouseDOMDelta.x / window.innerWidth,
+        this.iMouse.mouseDOMDelta.y / window.innerHeight
+      );
+
+      const mouseSpeed = Math.hypot(hoverDelta.x, hoverDelta.y);
+      targetSpeed = THREE.MathUtils.lerp(targetSpeed, mouseSpeed, 0.1);
+      customEffect.customPass.material.uniforms.uMouseSpeed.value = Math.min(
+        targetSpeed,
+        0.05
+      );
+      targetSpeed *= 0.999;
+    });
+  }
+}
