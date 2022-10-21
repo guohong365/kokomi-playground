21c7eee22eb5d11961219240b89fc789eb6a819a
alphardex <2582347430@qq.com>
2022-10-21 13:50:25 +0800

    update

--- entries/imageFullscreen/vert.glsl
+++ entries/imageFullscreen/vert.glsl
@@ -8,17 +8,57 @@ uniform vec2 uMeshSize;
 uniform vec2 uMeshPosition;
 uniform float uProgress;
 
+// progress
+// plain
+float getProgress1(float pr,vec2 uv){
+    return pr;
+}
+
+// stagger
+float getProgress2(float pr,vec2 uv){
+    float activation=uv.x;
+    float latestStart=.5;
+    float startAt=activation*latestStart;
+    pr=smoothstep(startAt,1.,pr);
+    return pr;
+}
+
+// transform
+// flip
+vec3 flipX(vec3 p,float pr){
+    p.x=mix(p.x,-p.x,pr);
+    return p;
+}
+
+vec2 flipUvX(vec2 uv,float pr){
+    uv.x=mix(uv.x,1.-uv.x,pr);
+    return uv;
+}
+
 vec3 fullscreen(vec3 p){
-    // Scale to view size
+    // copy uv
+    vec2 newUv=uv;
+    
+    // scale to view size
     vec2 scale=mix(vec2(1.),iResolution/uMeshSize,uProgress);
     p.xy*=scale;
     
-    // Move towards center
-    p.x+=-uMeshPosition.x*uProgress;
-    p.y+=-uMeshPosition.y*uProgress;
+    // get progress
+    float pr=getProgress2(uProgress,uv);
+    
+    // other transforms
+    p=flipX(p,pr);
+    newUv=flipUvX(newUv,pr);
+    
+    // get uv
+    vUv=newUv;
+    
+    // move to center
+    p.x+=-uMeshPosition.x*pr;
+    p.y+=-uMeshPosition.y*pr;
     
     // z
-    p.z+=uProgress;
+    p.z+=pr;
     
     return p;
 }
@@ -29,6 +69,4 @@ void main(){
     p=fullscreen(p);
     
     gl_Position=projectionMatrix*modelViewMatrix*vec4(p,1.);
-    
-    vUv=uv;
 }
