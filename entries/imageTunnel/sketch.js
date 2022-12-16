59b88b0435367c999c162d9c4875e6875b7cfe06
alphardex <2582347430@qq.com>
2022-12-16 16:11:00 +0800

    update

--- entries/imageTunnel/sketch.js
+++ entries/imageTunnel/sketch.js
@@ -30,48 +30,86 @@ class ImageTunnel extends kokomi.Component {
 
     const geo = new THREE.PlaneGeometry(10, 10);
     this.geo = geo;
-  }
-  addExisting() {
-    const meshs = this.urls.map((url) => {
-      const matClone = this.mat.clone();
-      const mesh = new THREE.Mesh(this.geo, matClone);
-      mesh.material.uniforms.uTexture.value = new THREE.TextureLoader().load(
-        url
-      );
-      this.base.scene.add(mesh);
-      return mesh;
-    });
-    this.meshs = meshs;
 
-    this.randomizePos();
+    this.meshs = [];
+
+    this.isRunning = false;
+  }
+  addImage(url) {
+    return new Promise((resolve) => {
+      new THREE.TextureLoader().load(
+        url,
+        (res) => {
+          const matClone = this.mat.clone();
+          const mesh = new THREE.Mesh(this.geo, matClone);
+          mesh.material.uniforms.uTexture.value = res;
+          this.base.scene.add(mesh);
+          this.meshs.push(mesh);
+          resolve(mesh);
+        },
+        () => {},
+        () => {
+          resolve(true);
+        }
+      );
+    });
+  }
+  async addImages(urls) {
+    await Promise.all(urls.map((url) => this.addImage(url)));
+  }
+  async addExisting() {
+    await this.addImages(this.urls);
+    this.emit("ready");
+    this.randomizeMeshesPos();
+    this.run();
   }
   update() {
     if (this.uj && this.mat && this.meshs) {
+      if (!this.isRunning) {
+        return;
+      }
       this.meshs.forEach((mesh) => {
         this.uj.injectShadertoyUniforms(mesh.material.uniforms);
-
         mesh.position.z = (mesh.position.z - 2) % 2000;
       });
     }
   }
-  randomizePos() {
+  getRandomXY() {
+    const theta = THREE.MathUtils.randFloat(0, 360);
+    const r = THREE.MathUtils.randFloat(10, 50);
+    const x = r * Math.cos(theta);
+    const y = r * Math.sin(theta);
+    return { x, y };
+  }
+  getRandomPos() {
+    const { x, y } = this.getRandomXY();
+    const z = THREE.MathUtils.randFloat(-1000, 1000);
+    return new THREE.Vector3(x, y, z);
+  }
+  randomizeMeshesPos() {
     if (this.meshs) {
       this.meshs.forEach((mesh) => {
-        const theta = THREE.MathUtils.randFloat(0, 360);
-        const r = THREE.MathUtils.randFloat(10, 50);
-        const x = r * Math.cos(theta);
-        const y = r * Math.sin(theta);
-        const z = THREE.MathUtils.randFloat(-1000, 1000);
-        mesh.position.x = x;
-        mesh.position.y = y;
-        mesh.position.z = z;
+        const randPos = this.getRandomPos();
+        mesh.position.copy(randPos);
       });
     }
+  }
+  run() {
+    this.isRunning = true;
+  }
+  stop() {
+    this.isRunning = false;
+  }
+  async addImageAtRandPos(url) {
+    const newMesh = await this.addImage(url);
+    const { x, y } = this.getRandomXY();
+    const newMeshPos = new THREE.Vector3(x, y, -900);
+    newMesh.position.copy(newMeshPos);
   }
 }
 
 class Sketch extends kokomi.Base {
-  create() {
+  async create() {
     const camera = new THREE.PerspectiveCamera(
       60,
       window.innerWidth / window.innerHeight,
@@ -85,13 +123,23 @@ class Sketch extends kokomi.Base {
     // new kokomi.OrbitControls(this);
 
     const urls = [...Array(100).keys()].map((item, i) => {
-      return `https://picsum.photos/id/${i}/200/300`;
+      return `https://picsum.photos/id/${i}/100/100`;
       // return `https://s2.loli.net/2022/09/08/gGY4VloDAeUwWxt.jpg`;
     });
 
     const at = new ImageTunnel(this, {
       urls,
     });
-    at.addExisting();
+    at.on("ready", () => {
+      document.querySelector(".loader-screen").classList.add("hollow");
+    });
+    await at.addExisting();
+
+    // test add image at rand pos
+    // await kokomi.sleep(1000);
+
+    // await at.addImageAtRandPos(
+    //   "https://s2.loli.net/2022/09/08/gGY4VloDAeUwWxt.jpg"
+    // );
   }
 }
