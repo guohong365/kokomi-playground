4db497648d17bda6ba3fcd2bf6ffed659188ec39
alphardex <2582347430@qq.com>
2022-12-16 16:20:11 +0800

    update

--- entries/imageTunnel/frag.glsl
+++ entries/imageTunnel/frag.glsl
@@ -15,10 +15,10 @@ float circle(vec2 st,float r,vec2 v){
 void main(){
     vec2 p=vUv;
     
-    vec4 col=texture(uTexture,p);
-    // vec4 col=vec4(p,0.,1.);
+    vec3 col=texture(uTexture,p).xyz;
+    // vec3 col=vec3(p,0.);
     
     float alpha=1.-circle(p,.5,vec2(.5));
     
-    gl_FragColor=vec4(col.xyz,alpha);
+    gl_FragColor=vec4(col,alpha);
 }
