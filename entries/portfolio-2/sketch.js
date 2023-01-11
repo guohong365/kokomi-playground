7d433ad862c49440fada80543d54eeb2b11d81b0
alphardex <2582347430@qq.com>
2023-01-11 08:45:46 +0800

    update

--- entries/portfolio-2/sketch.js
+++ entries/portfolio-2/sketch.js
@@ -241,37 +241,37 @@ class Sketch extends kokomi.Base {
         composer.addPass(new POSTPROCESSING.EffectPass(this.camera, dof));
 
         // ssr
-        const ssrEffect = new SSREffect(this.scene, this.camera, {
-          temporalResolve: true,
-          STRETCH_MISSED_RAYS: true,
-          USE_MRT: true,
-          USE_NORMALMAP: true,
-          USE_ROUGHNESSMAP: true,
-          ENABLE_JITTERING: true,
-          ENABLE_BLUR: true,
-          temporalResolveMix: 0.9,
-          temporalResolveCorrectionMix: 0.25,
-          maxSamples: 0,
-          resolutionScale: 1,
-          blurMix: 0.5,
-          blurKernelSize: 8,
-          blurSharpness: 0.5,
-          rayStep: 0.3,
-          intensity: 1,
-          maxRoughness: 0.1,
-          jitter: 0.7,
-          jitterSpread: 0.45,
-          jitterRough: 0.1,
-          roughnessFadeOut: 1,
-          rayFadeOut: 0,
-          MAX_STEPS: 20,
-          NUM_BINARY_SEARCH_STEPS: 5,
-          maxDepthDifference: 3,
-          maxDepth: 1,
-          thickness: 10,
-          ior: 1.45,
-        });
-        const ssrPass = new POSTPROCESSING.EffectPass(this.camera, ssrEffect);
+        // const ssrEffect = new SSREffect(this.scene, this.camera, {
+        //   temporalResolve: true,
+        //   STRETCH_MISSED_RAYS: true,
+        //   USE_MRT: true,
+        //   USE_NORMALMAP: true,
+        //   USE_ROUGHNESSMAP: true,
+        //   ENABLE_JITTERING: true,
+        //   ENABLE_BLUR: true,
+        //   temporalResolveMix: 0.9,
+        //   temporalResolveCorrectionMix: 0.25,
+        //   maxSamples: 0,
+        //   resolutionScale: 1,
+        //   blurMix: 0.5,
+        //   blurKernelSize: 8,
+        //   blurSharpness: 0.5,
+        //   rayStep: 0.3,
+        //   intensity: 1,
+        //   maxRoughness: 0.1,
+        //   jitter: 0.7,
+        //   jitterSpread: 0.45,
+        //   jitterRough: 0.1,
+        //   roughnessFadeOut: 1,
+        //   rayFadeOut: 0,
+        //   MAX_STEPS: 20,
+        //   NUM_BINARY_SEARCH_STEPS: 5,
+        //   maxDepthDifference: 3,
+        //   maxDepth: 1,
+        //   thickness: 10,
+        //   ior: 1.45,
+        // });
+        // const ssrPass = new POSTPROCESSING.EffectPass(this.camera, ssrEffect);
         // composer.addPass(ssrPass);
 
         this.composer = composer;
